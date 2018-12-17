a095fac66280f3b8f67598e3adbec7f8b73e991d
varun <bahl@octimine.com>
2018-12-17 10:22:04 +0100

    reorganizing

---
+++ src/ShallowLearning/NaiveBayes.py
@@ -0,0 +1,5 @@
+class NaiveBayes:
+
+    def __init__(self):
+        pass
+
--- src/ShallowLearning/SupportVectorMachine.py
+++ src/ShallowLearning/SupportVectorMachine.py
@@ -1,9 +1,3 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn import svm
-
 svm_tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
                      'C': [1, 10, 100, 1000]},
                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
@@ -35,38 +29,3 @@ class SVM:
         svm.SVC(C = C_Penalty,kernel = s_kernel,degree = s_degree, cache_size = s_kernel_cache_size, tol = s_tolerance,
                 class_weight = s_class_weight,gamma=s_gamma
                 )
-class LogisticRegression:
-
-    def __init__(self):
-        pass
-
-
-class RandomForst:
-
-    def __init__(self):
-        pass
-
-
-def train_SVM_classifier(x,y):
-
-    # Fit basic SVM and get a feel for the results, then we can add the others
-    clf = GridSearchCV(estimator=SVC(), param_grid=parameter_candidates)
-
-    svm.SVC(kernel=mkernel, degree=mdegree, cache_size=mcache_size, tol=mtol, class_weight=mclass_weight, C=mC,
-            gamma=mgamma)
-
-def train_logistic_Regression(x,y):
-    pass
-
-
-def train_random_forest(x,y):
-    pass
-
-
-def train_naive_bayes(x,y):
-
-
-
-
-
-
