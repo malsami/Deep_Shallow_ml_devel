5776e6127d5d546fa12af2f133509b860e1aca51
varun <bahl@octimine.com>
2018-12-19 12:50:34 +0100

    torch documentation and some reformatting

--- models/DeepLearning/FFN.py
+++ models/DeepLearning/FFN.py
@@ -4,12 +4,55 @@ import torch.nn.functional as F
 
 class FFN(nn.Module):
     def __init__(self, input_size, hidden_size_1, hidden_size_2, num_classes):
+        """
+        Class for Troch FFN implemented by Robert during Bachelor's thesis. Initiation function for graph.
+
+        ...
+
+        Attributes
+        ----------
+        input_size: int
+
+            First layer of the neural network corresponds to the size (dimension) of the training data
+
+        hidden_size_1: int
+
+            Hidden layer size chosen by user
+
+        hidden_size_2: int
+
+            Hidden layer size chosen by user
+
+        num_classes: int
+
+            Number of classes will determine the final layer and respective activation function
+
+        Methods
+        -------
+
+        forward(x)
+            Forward pass of torch model. Will define how data is propogated and computed on in the graph.
+        """
         super().__init__()
         self.h1 = nn.Linear(input_size, hidden_size_1)
         self.h2 = nn.Linear(hidden_size_1, hidden_size_2)
         self.h3 = nn.Linear(hidden_size_2, num_classes)
 
     def forward(self, x):
+        """
+        Forward pass for pytorch model which computes on x (training data) as it is propogated through network.
+
+
+        Parameters
+        ----------
+        x : numpy array [N x D]
+            training set samples where n is number of samples and d is the dimension of each sample
+
+        y : numpy array [N x 1]
+            training set labels where each sample is either '1' or '0'.
+
+        """
+
         x = self.h1(x)
         x = F.leaky_relu(x)
         x = self.h2(x)
