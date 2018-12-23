d7e7dcdc28bf4131f091e9843c854b44829b32c1
varun <bahl@octimine.com>
2018-12-23 11:02:34 +0100

    some more shallow learning

--- models/ShallowLearning/KNearestNeighbors.py
+++ models/ShallowLearning/KNearestNeighbors.py
@@ -1,17 +1,26 @@
 from sklearn.neighbors import KNeighborsClassifier
 import logging
 
-class KNN:
+# Super Class
+from models.ShallowLearning import Shallow_Model
+
+class KNN(Shallow_Model):
 
     model = None
 
-    def __init__(self, num_neighbors, k_weights, k_algorithm):
+    def __init__(self, num_neighbors, k_weights = 'uniform', k_algorithm='auto'):
+        super(KNN, self).__init__(name="KNN with " + str(num_neighbors) + " neighbors")
         self.model = KNeighborsClassifier(n_neighbors=num_neighbors, weights=k_weights, algorithm=k_algorithm)
 
     def train(self, x,y):
-        # self.model.fit(x,y)
-        self.model.fit(x,y)
+
+        self.model.fit(x)
         logging.info("KNN is training")
+
+
+    def predict(self,x):
+        pass
+
 
     def analyze(self):
         pass
--- models/ShallowLearning/NaiveBayes.py
+++ models/ShallowLearning/NaiveBayes.py
@@ -1,5 +1,102 @@
-class NaiveBayes:
+# Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+
+from models.ShallowLearning import Shallow_Model
+
+
+class GaussianNaiveBayes(Shallow_Model):
+    """
+    GaussianNB wrapper function. Child of Shallow Model
+
+
+
+    Attributes
+    ----------
+    model : GaussianNB from sci-kit learn
+        Naive Bayes with normal/gaussian distribution
+
+    Methods
+    -------
+    train(x,y)
+        fits the model with sci-kit learn's 'fit' funciton
+    predict(x)
+        predicts values for new data 'x'
+
+    """
+    model = None
 
     def __init__(self):
-        pass
+        super(GaussianNaiveBayes, self).__init__(name="Gaussian Bayes")
+        self.model = GaussianNB()
 
+    def train(self, x, y):
+        super(GaussianNaiveBayes, self).train(x, y)
+
+    def predict(self, x):
+        return super(GaussianNaiveBayes, self).predict(x)
+
+
+class MultinomialNaiveBayes(Shallow_Model):
+    """
+    MultinomialNB wrapper function. Child of Shallow Model
+
+
+
+    Attributes
+    ----------
+    model : MultinomialNB from sci-kit learn
+        Multinomial Bayes with multinomial gaussian predictions
+
+    Methods
+    -------
+    train(x,y)
+        fits the model with sci-kit learn's 'fit' funciton
+    predict(x)
+        predicts values for new data 'x'
+
+    """
+    model = None
+
+    def __init__(self):
+        super(MultinomialNaiveBayes, self).__init__(name="Multinomial Bayes")
+        self.model = MultinomialNB()
+
+    def train(self, x, y):
+        super(MultinomialNaiveBayes, self).train(x, y)
+
+    def predict(self, x):
+        return super(MultinomialNaiveBayes, self).predict(x)
+
+
+class BernoulliNaiveBayes(Shallow_Model):
+    """
+        BernoulliNB wrapper function. Child of Shallow Model
+
+
+
+    Attributes
+    ----------
+    model : BernoulliNB from sci-kit learn
+        Bernoulli Bayes with multinomial gaussian predictions
+
+    Methods
+    -------
+    train(x,y)
+        fits the model with sci-kit learn's 'fit' funciton
+    predict(x)
+        predicts values for new data 'x'
+
+    """
+    model = None
+
+    def __init__(self):
+        super(BernoulliNaiveBayes, self).__init__(name="BernoulliNaiveBayes")
+        self.model = BernoulliNB()
+
+    def train(self, x, y):
+        super(BernoulliNaiveBayes, self).train(x, y)
+
+    def predict(self, x):
+        return super(BernoulliNaiveBayes, self).predict(x)
