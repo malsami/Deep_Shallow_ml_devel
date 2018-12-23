a3afde0778567b63974f2343124c3120c5eebc2d
varun <bahl@octimine.com>
2018-12-23 09:01:41 +0100

    hyper tuning

--- src/Trainer.py
+++ src/Trainer.py
@@ -9,26 +9,50 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 import torch.utils.data as data_utils
 
-from src.Utils import load_data
+from src.Utils import load_data, train_val_test_split
 
 #Tuning
 from src.Tuning import Solver
 
 path = 'C:\\Users\\Varun\\Documents\\Misc\\Research\\MalSami\\'
 
+# Splits
+train_split = .6
+val_split = .2
+test_split = .2
+
+assert(train_split + val_split + test_split == 1)
 
 
+def train_model_shallow(model, x,y):
+    """
+    Class that will tune the model via training and validation data. This model makes use of the solver model (taken
+    from TUM i2DL/Stanford cs231n class in which a standarad model is used to tune and effectively fit the network
+    effectively
 
-def train_model(x,y,split_size):
+
+    Parameters
+    ----------
+    model: Torch model
+        Torch model to optimize
+
+    x : numpy array [N x D]
+        training set samples where n is number of samples and d is the dimension of each sample
+
+    y : numpy array [N x 1]
+        training set labels where each sample is either '1' or '0'.
+
+    """
+
     # Split the data
-    x_train, x_test, y_train,y_test = train_test_split(x, y, test_size=split_size, random_state=42)
+    x_train, y_train, x_val, y_val, x_test, y_test = train_val_test_split(x = x.numpy(), y = y.numpy(), to_tensor=True)
 
-    shallow_learning_model = KNN(3,'uniform','auto')
+    # shallow_learning_model = KNN(3, 'uniform', 'auto')
 
-    shallow_learning_model.train(x,y)
+    # shallow_learning_model.train(x_train, y_train)
 
 
-def torch_model_trainer(x,y, batch_size):
+def torch_model_trainer(x,y, batch_size = 64):
     """TODO ADD RECURRENT NEURAL NETWORK BY INDIAN STUDENT"""
 
     input_size = x.shape[1]
@@ -42,7 +66,7 @@ def torch_model_trainer(x,y, batch_size):
     logging.info("Model created and using CUDA support")
 
 
-def torch_optimizer(model, x,y, batch_size = 64):
+def torch_optimizer(model, x, y, batch_size=64):
     """
     Class that will tune the model via training and validation data. This model makes use of the solver model (taken
     from TUM i2DL/Stanford cs231n class in which a standarad model is used to tune and effectively fit the network
@@ -51,6 +75,9 @@ def torch_optimizer(model, x,y, batch_size = 64):
 
     Parameters
     ----------
+    model: Torch model
+        Torch model to optimize
+
     x : numpy array [N x D]
         training set samples where n is number of samples and d is the dimension of each sample
 
@@ -61,16 +88,21 @@ def torch_optimizer(model, x,y, batch_size = 64):
         batch_size of the data. Recommendations are (32,64,128..) the most you can to max out the power of the GPU
 
     """
-    # DataLoader for efficient data loading and easy batching
-    train_tensor = data_utils.TensorDataset(x,y)
+    # Splits are by default .6 train, .2 val, .2 test
+    x_train, y_train, x_val, y_val, x_test, y_test = train_val_test_split(x=x.numpy(), y=y.numpy(), to_tensor=True)
+
+    # For data loading
+    train_tensor = data_utils.TensorDataset(x_train,y_train)
+    val_tensor = data_utils.TensorDataset(x_val, y_val)
 
     train_data = DataLoader(train_tensor, batch_size=batch_size, shuffle=True, num_workers=8)
+    val_data = DataLoader(val_tensor, batch_size=batch_size, shuffle=True, num_workers=8)
 
     logging.info("Data model loaded and ready for hyperparameter tuning")
     # Put into the solver module
     solver = Solver()
 
-    solver.train(model,)
+    solver.train(model, train_data, val_data)
 
 
 def tensorflow_trainer(x,y):
@@ -89,5 +121,9 @@ if __name__ == "__main__":
     logging.info("Logger started")
     x_tensor, x, y_tensor, y = load_data()
     # train_model(x, y, .01)
-    torch_model_trainer(x_tensor,y_tensor)
 
+
+    # Shallow Models
+
+    shallow_model = KNN(3, 'uniform', 'auto')
+    train_model_shallow()
