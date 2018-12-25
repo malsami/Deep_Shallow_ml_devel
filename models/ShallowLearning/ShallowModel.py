8be2b07ded0d74a76d42427506d5aa8e9a637518
varun <bahl@octimine.com>
2018-12-25 13:09:38 +0100

    grid search in Shallow Class

--- models/ShallowLearning/ShallowModel.py
+++ models/ShallowLearning/ShallowModel.py
@@ -1,7 +1,10 @@
 import logging
 
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 
-class Shallow_Model():
+
+class ShallowModel:
     """
     Parent (abstract) class for shallow learning models
 
@@ -20,27 +23,61 @@ class Shallow_Model():
         predicts values for new data 'x'
 
     """
-    model = None  # MultinomialNB()
+    model = None
+    hyperparameters = None
+
 
     def __init__(self, name):
-        print("Shallow Model Created: ", name)
+        logging.info("Shallow Model Created: ", name)
 
     def train(self, x, y):
         self.model.fit(x, y)
 
     def predict(self, x, log_predict=False):
-        if (log_predict):
+        if log_predict:
             return self.model.predict_log_proba()
         else:
             return self.model.predict(x)
 
-    def k_fold_fit(self, x, y):
-        """
-        K fold cross validation for finding fit of data
-        :param x:
-        :param y:
-        :return:
+    def validate(self, x, y, num_folds=10):
+        """[K fold] cross validation for evaluating the hyperparameters
+        Parameters
+        ----------
+
+        x: Array [N x D]
+            Training samples
+        y: Array [N]
+            Training labels
+        num_folds: int
+            Number of folds for cross validation, 10 or fewer is recommended
+
         """
 
-    def optimize(self):
-        pass
+        kfold = KFold(num_folds, True)
+
+        for train, test in kfold.split(data):
+            print('train: %s, test: %s' % (data[train], data[test]))
+
+    def optimize(self, x, y, cv, verbose=0):
+        """Grid Search for finding best hyperparameters. WARNING. This may take a long while and may not be optimal.
+
+        Parameters
+        ----------
+        x : numpy array [N x D]
+            training set samples where n is number of samples and d is the dimension of each sample
+
+        y : numpy array [N x 1]
+            training set labels where each sample is either '1' or '0'.
+
+        cv: int
+            Number of folds for k fold validation
+
+        verbose: int optional
+            Whether or not we want to display the gridsearch. '1' for yes, '0' for no.
+        """
+
+        glf = GridSearchCV(self.model, self.hyperparameters, cv=cv)
+        best_model = glf.fit(x, y)
+
+        return best_model
+
